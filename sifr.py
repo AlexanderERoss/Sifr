f65dfe68064280b2a9f4c83cd1e1d0e7e70d553e
Alexander Ross <alexander.eric.ross@gmail.com>
2017-11-30 12:15:58 +1100

    Version 0.0.0.3
    Added the arithmetic files, yet incomplete.

--- sifr.py
+++ sifr.py
@@ -1,8 +1,20 @@
-#Calculates numbers in a given number system
-#Alexander Ross
-#(C) 2017
-#alexander.eric.ross@gmail.com
-#########################################################
+################################################################################
+#Created by Alexander Ross
+#25th Novewmber 2017
+################################################################################
+# Sifr is the main script which calculates the main arithmetic of the
+# generalised number system.
+################################################################################
 
-import math
+import sifrisation
+
+class sifr(object):
+    def __init__(self, digit_list):
+        self.digit_list = digit_list
+
+    def calcul(self, operation_string):
+        '''Takes a string and parses it as a formula'''
+        self.operation_string = operation_string
+
+sifrisation.sifrise() #Needs to be added
 
